40f5cf215a379e06f77d565e342d80936179867b
brenoavidos <breno.avidos@gmail.com>
2024-10-10 12:10:14 -0300

    Dicionario da pnad organizado
    Em 4 colunas: variável, descrição (da variável), valor (só com números discretos) e label (da variável)

---
+++ PNAD_Continua/dicionario_pnadc.R
@@ -0,0 +1,17 @@
+library(readxl)
+library(tidyverse)
+
+#Importando o dicionario
+dicionario_das_variaveis_PNAD_Continua_microdados <- 
+  read_excel("~/GitHub/dicionario_das_variaveis_PNAD_Continua_microdados.xls")
+
+#Selecionando as colunas referentes ao Codigo e Tipo da Variavel
+dic_pnadc <- dicionario_das_variaveis_PNAD_Continua_microdados[, c(3, 5, 6, 7)]
+dic_pnadc <- dic_pnadc %>% rename(c(Variavel = ...3, Descricao = ...5, Valor = ...6, Label = ...7))
+dic_pnadc <- dic_pnadc %>% tail(-3)
+dic_pnadc <- dic_pnadc %>% fill(c(Variavel, Descricao))
+dic_pnadc <- dic_pnadc %>% filter(!is.na(Valor) | !is.na(Label))
+
+#Deixando apenas numeros discretos na coluna Valor
+dic_pnadc$Valor <- as.numeric(dic_pnadc$Valor)
+dic_pnadc <- dic_pnadc %>% filter(!is.na(Valor))
