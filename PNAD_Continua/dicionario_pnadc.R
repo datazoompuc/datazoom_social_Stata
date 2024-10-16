8ad093f9abc41cb358ce55eabb535cc1faee8e34
IgorRigolon <77173467+IgorRigolon@users.noreply.github.com>
2024-10-16 13:37:24 -0300

    updating value labels R script

--- PNAD_Continua/dicionario_pnadc.R
+++ PNAD_Continua/dicionario_pnadc.R
@@ -1,14 +1,31 @@
+# Script to generate value labels for PNADC variables
+# It automatically reads the IBGE dictionary, parses it
+# and writes Stata code
+
+# Packages
+
 library(readxl)
 library(stringr)
 library(tidyverse)
 library(tidyr)
 
+# Defining path for the IBGE dictionary
+message("Type the path of the IBGE PNADC dictionary in .xls")
+
+ibge_path <- readline()
+
+# Choosing whether to translate to English
+message("Which language? pt or en?")
+
+lang <- readline()
+
+## Starting manipulation
+
 #Importando o dicionario
-dicionario_das_variaveis_PNAD_Continua_microdados <- 
-  read_excel("~/GitHub/dicionario_das_variaveis_PNAD_Continua_microdados.xls")
+dic_pnadc <- read_excel(ibge_path)
 
 #Selecionando as colunas referentes ao Codigo e Tipo da Variavel
-dic_pnadc <- dicionario_das_variaveis_PNAD_Continua_microdados[, c(3, 5, 6, 7)]
+dic_pnadc <- dic_pnadc[, c(3, 5, 6, 7)]
 dic_pnadc <- dic_pnadc %>% rename(c(Variavel = ...3, Descricao = ...5, Valor = ...6, Label = ...7))
 dic_pnadc <- dic_pnadc %>% tail(-3)
 dic_pnadc <- dic_pnadc %>% fill(c(Variavel, Descricao))
@@ -17,6 +34,13 @@ dic_pnadc <- dic_pnadc %>% filter(!is.na(Valor) | !is.na(Label))
 #Deixando apenas numeros discretos na coluna Valor
 dic_pnadc$Valor <- as.numeric(dic_pnadc$Valor)
 dic_pnadc <- dic_pnadc %>% filter(!is.na(Valor))
+
+vars_list <- dic_pnadc$Variavel %>%
+  unique()
+
+# fixing some strings
+dic_pnadc$Label <- dic_pnadc$Label %>%
+  gsub("\n", " ", .)
 
 #Juntando as colunas Valor e Label
 dic_pnadc <- dic_pnadc %>% mutate(Label = paste0("\"", Label, "\""))
@@ -30,5 +54,25 @@ dic_pnadc <- dic_pnadc %>% group_by(Variavel) %>%
 dic_pnadc <- dic_pnadc %>% mutate(Variavel = paste0("label_", Variavel, sep = ""))
 dic_pnadc <- dic_pnadc %>% mutate(Label = str_c(Variavel, Label, sep = " "))
 dic_pnadc <- dic_pnadc %>% mutate(Label = paste0("label define ", Label, sep = " "))
-dic_pnadc <- dic_pnadc %>% select(Label)
-writeLines(dic_pnadc$Label, "bbbb.txt")
+dic_pnadc <- dic_pnadc$Label
+
+# attributing labels
+
+vars_list <- paste0("cap label values ", vars_list, " label_", vars_list)
+
+# combining
+
+dic_pnadc <- c(
+  "program define pnadc_value_labels",
+  dic_pnadc,
+  vars_list,
+  "end"
+)
+
+# Choose where to save file
+
+message("Folder to save the code")
+
+save_to <- readline()
+
+writeLines(dic_pnadc, save_to)
